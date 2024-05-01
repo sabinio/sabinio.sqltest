bf566dd330eca81aa725979eb95782144e762289
Simon Sabin <Simon@sqlbits.com>
2024-05-01 10:17:04 +0100

    fix testing

--- .build/test.ps1
+++ .build/test.ps1
@@ -5,7 +5,7 @@ try{
 
     foreach($lib in "SabinIO.Sql.Parse.Tests","SabinIO.SqlTest.Tests","SabinIO.xEvent.Lib.Tests"){
 
-        &dotnet test .\src\$lib  -s .\src\test.runsettings --no-build  -r ".\out\results\tests.$($lib)"  --logger:console --logger:trx
+        &dotnet test .\src\$lib  -s .\src\test.runsettings --no-build   --results-directory ".\out\results\tests.$($lib)"  --logger:console --logger:trx
     }
     
 }
